721b1734560781400f4878dc727467a942771c57
Anshuman Verma <tanshuman118@gmail.com>
2026-08-03 00:00:32 +0530

    Add Tailwind config

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        mint: '#14b8a6',
+        coral: '#f97316',
+        plum: '#7c3aed',
+        ink: '#1f2937'
+      }
+    }
+  },
+  plugins: []
+};
